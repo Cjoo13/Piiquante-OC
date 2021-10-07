a95a092c0c97b6a0437f4690c752bce2db3293d8
Cjoo13 <sejourneremy@gmail.com>
2021-10-07 15:47:25 +0200

    problème connexion mongo db

--- Backend/routes/sauce.js
+++ Backend/routes/sauce.js
@@ -8,8 +8,7 @@ const sauceCtrl = require("../controllers/sauce");
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post("/", auth, multer, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, sauceCtrl.updateSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
-router.post("/:id/like", auth, sauceCtrl.likeDislikeSauce);
 
 module.exports = router;
